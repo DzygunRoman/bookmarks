8b9f036fdffdde5068553a1588fbf777c2f81ed9
DzygunRoman <dzygun-roman@mail.ru>
2024-01-27 16:47:49 +0300

    изменил пути

--- account/urls.py
+++ account/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     # path('password-reset/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_reset_done'),
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    #
+
 
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
