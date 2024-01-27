69c9aeaf8a4d8e3e057ee979f9edb6b491fd3d34
DzygunRoman <dzygun-roman@mail.ru>
2024-01-27 16:58:16 +0300

    метод сопоставления двйного ввода пароля

--- account/forms.py
+++ account/forms.py
@@ -14,3 +14,10 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password']
