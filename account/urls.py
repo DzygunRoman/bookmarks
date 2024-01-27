4e384331972d6112b59c8782a8469c19d60150cb
DzygunRoman <dzygun-roman@mail.ru>
2024-01-27 16:12:07 +0300

    шаблон проверяющий валидность ссылки на сброс пароля

--- account/urls.py
+++ account/urls.py
@@ -7,18 +7,18 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
     # URL-адреса входа и выхода
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    # URL-адреса смены пароля
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    # URL-адреса сброса пароля
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    path('', views.dashboard, name='dashboard'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #
+    # # URL-адреса смены пароля
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #
+    # # URL-адреса сброса пароля
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #
+    # path('', views.dashboard, name='dashboard'),
 ]
