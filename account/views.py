812236ff72b40b84de359af1a4d266bb9aa88f2d
DzygunRoman <dzygun-roman@mail.ru>
2024-01-29 11:48:43 +0300

    При регистрации пользователя в системе будет создан объект Profile ассоциированный с созданным объектом User дописываю views.py метод register

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 @login_required # проверяет аутентификацию текущего пользователя
@@ -41,7 +42,11 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])#хешируем пароль
             #Сохранить объект User
             new_user.save()
+            #Создать профиль пользователя
+            Profile.user.create(user=new_user)#При регистрации пользователя в системе будет создан объект Profile ассоциированный с созданным объектом User
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
+
