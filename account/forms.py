4f095270770ff4763866df53234ac185b62670fe
DzygunRoman <dzygun-roman@mail.ru>
2024-01-29 11:40:42 +0300

    создаю две формы для редактирования пользователем имени фамилии почты дня рождения фото в файле forms.py class UserEditForm и ProfileEditForm

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -15,9 +16,20 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'email']
 
-
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Пароли не совпадают')
         return cd['password']
+
+
+class UserEditForm(forms.ModelForm):# Эта форма позволит редактировать пользователям свое имя, фамилию и адрес почты
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):# Позволит редактировать пользователям данные конкретно-прикладной модели Profile дату рождения и фото
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
