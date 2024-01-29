c871ee18b6a0c06f520abb012a325797f6e29be5
DzygunRoman <dzygun-roman@mail.ru>
2024-01-29 10:53:14 +0300

    Регистрирую модель Profile на сайте администрирования в файле admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)# Регистрирую модель Profile на сайте администрирования
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
+    raw_id_fields = ['user']
+
+
+
