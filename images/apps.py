59d8fa0bafcf311c5d4beda3adf9070b4513408c
DzygunRoman <dzygun-roman@mail.ru>
2024-01-30 13:24:10 +0300

    создаю приложение images  регистрирую и в ней модель Image

--- images/apps.py
+++ images/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
+    verbose_name = "Изображение"#название приложения в админ-панели
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
