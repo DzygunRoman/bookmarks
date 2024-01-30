cee849bdaf39cd198c15eb97da6ff4bb8c416303
DzygunRoman <dzygun-roman@mail.ru>
2024-01-30 09:42:51 +0300

    добавил в представление edit messages об успеном обновлении профиля и об ошибке обновления

--- account/views.py
+++ account/views.py
@@ -4,11 +4,12 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
-@login_required # проверяет аутентификацию текущего пользователя
+@login_required  # проверяет аутентификацию текущего пользователя
 def dashboard(request):
-    return render(request,  'account/dashboard.html', {'section': 'dashboard'})
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def user_login(request):
@@ -36,28 +37,32 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            #Создать новый объект пользователя но пока не сохранять его
+            # Создать новый объект пользователя но пока не сохранять его
             new_user = user_form.save(commit=False)
-            #Установить выбранный пароль
-            new_user.set_password(user_form.cleaned_data['password'])#хешируем пароль
-            #Сохранить объект User
+            # Установить выбранный пароль
+            new_user.set_password(user_form.cleaned_data['password'])  # хешируем пароль
+            # Сохранить объект User
             new_user.save()
-            #Создать профиль пользователя
-            Profile.objects.create(user=new_user)#При регистрации пользователя в системе будет создан объект Profile ассоциированный с созданным объектом User
+            # Создать профиль пользователя
+            Profile.objects.create(
+                user=new_user)  # При регистрации пользователя в системе будет создан объект Profile ассоциированный с созданным объектом User
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
 
 
-@login_required#только аутентифицированные пользователи могут редактировать свою информацию
-def edit(request):# пользователь редактирует свою личную инфомацию
+@login_required  # только аутентифицированные пользователи могут редактировать свою информацию
+def edit(request):  # пользователь редактирует свою личную инфомацию
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Профиль успешно обновлен')
+        else:
+            messages.error(request,'Ошибка обновления вашего профиля')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
