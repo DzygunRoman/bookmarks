ffa250e0bf8c71f647642e7e6d71ba9e8d28c152
DzygunRoman <dzygun-roman@mail.ru>
2024-01-31 09:19:28 +0300

    хранение изображений на сайте представление image_create

--- images/views.py
+++ images/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        #Форма отправлена
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            #Данные в форме валидны
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            #Назначить текущего пользователя элементу
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Изображение успешно добавлено!')
+            #Перенаправить к представлению детальной информации о только что созданном элементе
+            return redirect(new_image.get_absolute_url())
+        else:
+            #скомпоновать форму с данными, представленными букмарклетом методом GET
+            form = ImageCreateForm(data=request.GET)
+        return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
+
+
